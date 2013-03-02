2b0e0807c1b626f7ca6b6f2cab5d5b5e281cacf2
Dave Woodall <dave.woodall@factorylabs.com>
2013-03-02 11:07:49 -0700

    Remove unneeded helper method

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,12 +1,4 @@
 module ItemsHelper
-  def total_items_available
-    if @item.trackable? 
-      @serials.count - @serials.where(user_id: 1).count
-    else
-      @item.quantity
-    end
-  end
-
   def link_to_user(item)
     link_to item.user.name, user_path(item.user) unless item.user.nil?
   end        
